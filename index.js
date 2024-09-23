5d7aa890d668893902d86d454235825f0dc27000
Виктория Давиденко <vikodavido@gmail.com>
2024-09-23 11:34:53 +0300

    lesson-1

---
+++ index.js
@@ -0,0 +1,12 @@
+
+console.log('----- 1 -----');
+let strLength = "Node.js course";
+console.log(strLength.length); 
+console.log('-------------');
+console.log('----- 2 -----');
+function multiplyByTwoNTimes(num) {
+  return (num * 2) * num.toString().length;
+}
+console.log(multiplyByTwoNTimes(33))
+console.log('-------------');
+
